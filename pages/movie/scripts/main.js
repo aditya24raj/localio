3b93c4c2c92508d66a575b1c2c73b4a2141241f0
aditya raj <aditya2400raj@gmail.com>
2026-03-14 11:06:21 +0530

    remove /pages

--- pages/movie/scripts/main.js
+++
@@ -1,1 +0,0 @@
-const BASE_URL_TORRENTIO = "https://torrentio.strem.fun/providers=yts,eztv,rarbg,1337x%7Cqualityfilter=brremux,hdrall,dolbyvision,dolbyvisionwithhdr,threed,4k,other,scr,cam,unknown%7Climit=1";
